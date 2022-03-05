809939fce772c1b09849316dfa9e8cc11935342d
Roman Ac <roman25xx@gmail.com>
2022-03-05 15:33:57 +1100

    schema.sql

--- db/schema.sql
+++ db/schema.sql
@@ -8,3 +8,20 @@ CREATE TABLE departments(
   PRIMARY KEY (id)
 );
 
+CREATE TABLE roles(
+  id INTEGER(11) AUTO_INCREMENT NOT NULL,
+  title VARCHAR(30) NOT NULL,
+  salary decimal(12, 2) NOT NULL,
+  depId INTEGER (11) NOT NULL, 
+  PRIMARY KEY (id) 
+);
+
+CREATE TABLE employees(
+  id INTEGER(11) AUTO_INCREMENT NOT NULL,
+  firstName VARCHAR(30) NOT NULL,
+  lastName VARCHAR(30) NOT NULL,
+  roleId INTEGER (11) NOT NULL,
+  managerId INTEGER(11),
+  PRIMARY KEY (id)
+);
+
