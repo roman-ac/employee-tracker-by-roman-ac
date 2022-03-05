5188cfd50ed0fbc52e45c3f7010ba06b2c585219
Roman Ac <roman25xx@gmail.com>
2022-03-05 21:09:23 +1100

    .sql files

--- db/schema.sql
+++ db/schema.sql
@@ -1,27 +1,30 @@
+
 DROP DATABASE IF EXISTS employee_db;
+
 CREATE DATABASE employee_db;
+
 USE employee_db;
 
 CREATE TABLE departments(
-  id INTEGER(11) AUTO_INCREMENT NOT NULL,
-  department VARCHAR(30) NOT NULL,
+  id INT AUTO_INCREMENT NOT NULL,
+  dept_name VARCHAR(30) NOT NULL,
   PRIMARY KEY (id)
 );
 
 CREATE TABLE roles(
-  id INTEGER(11) AUTO_INCREMENT NOT NULL,
+  id INT AUTO_INCREMENT NOT NULL,
   title VARCHAR(30) NOT NULL,
   salary decimal(12, 2) NOT NULL,
-  depId INTEGER (11) NOT NULL, 
+  dep_id INT, 
   PRIMARY KEY (id) 
 );
 
 CREATE TABLE employees(
-  id INTEGER(11) AUTO_INCREMENT NOT NULL,
-  firstName VARCHAR(30) NOT NULL,
-  lastName VARCHAR(30) NOT NULL,
-  roleId INTEGER (11) NOT NULL,
-  managerId INTEGER(11),
+  id INT AUTO_INCREMENT NOT NULL,
+  first_name VARCHAR(30) NOT NULL,
+  last_name VARCHAR(30) NOT NULL,
+  role_id INT NOT NULL,
+  manager_id INT,
   PRIMARY KEY (id)
 );
 
---
+++ db/seed.sql
@@ -0,0 +1,24 @@
+USE employee_db;
+
+INSERT INTO departments (dept_name)
+VALUES  ("Sales"),
+        ("Engineering"),
+        ("Finance"),
+        ("Legal");
+
+INSERT INTO roles (title, salary, dep_id)
+VALUES  ("Sales Lead", 100000, 1),
+        ("Lead Engineer", 150000, 2),
+        ("Software Engineer", 120000, 2),
+        ("Accountant", 125000, 3),
+        ("Legal Team Lead", 250000, 4);
+
+INSERT INTO employees (first_name, last_name, role_id, manager_id)
+VALUES  ("John", "Rambo", 1, null),
+        ("Mike", "Muoio", 2, 1),
+        ("Natalie", "Rodriguez", 3, 2),
+        ("David", "Hunt", 4, 3),
+        ("Alia", "White", 5, null),
+        ("Sarah", "Larkin", 2, null),
+        ("Tom", "Hollant", 4, 7),
+        ("Christian", "Flett", 1, 2);
